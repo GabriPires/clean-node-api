670401051abb0afe0702642612736429211bd300
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-15 17:38:51 -0300

    feat: ensure sign up controller returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,25 +1,32 @@
-import { z } from 'zod'
-import { MissingParamError } from '../errors/missing-param-error'
+import {
+  InvalidParamError,
+  MissingParamError,
+} from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
-
-const bodySchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-  passwordConfirmation: z.string(),
-})
+import { EmailValidator } from '../protocols/email-validator'
 
 export class SignUpController implements Controller {
-  handle(httpRequest: HttpRequest): HttpResponse {
-    const result = bodySchema.safeParse(httpRequest.body)
+  private readonly emailValidator: EmailValidator
 
-    if (result.success === false) {
-      const { error } = result
-      return badRequest(
-        new MissingParamError(error.issues[0].path[0] as string),
-      )
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
