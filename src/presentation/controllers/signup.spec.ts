71f5bb822babfff66d244d79ac87bc45c21eedbd
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-14 21:37:35 -0300

    test: ensure sign up controller return an error if email is not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,4 +18,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'John Doe',
+        // email: 'email@email.com',
+        password: 'password123',
+        passwordConfirmation: 'password123',
+      },
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
