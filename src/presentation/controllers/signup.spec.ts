d5cf165dbf3326f138acdf16f8a9e9df72216fbc
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-14 21:56:01 -0300

    test: ensure sign up controller return an error if no password has provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -39,4 +39,23 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toBeInstanceOf(MissingParamError)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'John Doe',
+        email: 'email@email.com',
+        // password: 'password123',
+        passwordConfirmation: 'password123',
+      },
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toBeInstanceOf(MissingParamError)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
